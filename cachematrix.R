eda3adcf76e63c4b97ba8233f9b844b1ce9584d5
Alan Ponte <alanjponte@gmail.com>
2014-07-17 19:57:35 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a cache of the Matrix X.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <<- x
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse of X, where X is a square matrix
+## (N rows and N columns).
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+    numrows = nrow(x)
+    numcols = nrow(x)
+    identity = diag(numrows)
+    
+    # If X is not square, we cannot take the inverse.
+    if (numrows != numcols) {
+        print("Matrix not square")
+        reutrn
+    }
+    
+    mat = makeCacheMatrix(x)
+    solve(mat)
 }
